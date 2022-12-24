948cae6f8b011a709a78359fc25f2158ba508279
victor <victor.klapholz@gmail.com>
2022-12-24 22:20:14 +0200

    uow: HandleCommand

--- pkg/adapters/repository.go
+++ pkg/adapters/repository.go
@@ -1,24 +1,14 @@
 package adapters
 
-import "github.com/vklap.go-ddd/pkg/domain"
+import (
+	"context"
+	"github.com/vklap.go-ddd/pkg/domain"
+)
 
-type EntityFinder[TEntity domain.Entity] interface {
-	Find(id domain.Identifier) (*TEntity, error)
-}
-
-type EntitySaver[TEntity domain.Entity] interface {
-	Save(entity *TEntity) error
-}
-
-type Committer[TEntity domain.Entity] interface {
-	Commit() (savedEntities []TEntity, err error)
-}
-
-type Rollbacker interface {
-	Rollback() error
-}
-
-type Repository[TEntity domain.Entity] interface {
-	Committer[TEntity]
-	Rollbacker
+type Repository[Entity domain.Entity] interface {
+	Find(ctx context.Context, id string) (Entity, error)
+	Save(ctx context.Context, entity Entity) error
+	Commit(ctx context.Context) error
+	Rollback(ctx context.Context) error
+	SavedEntities() []Entity
 }
--- pkg/domain/model.go
+++ pkg/domain/model.go
@@ -1,27 +1,52 @@
 package domain
 
+type EventType string
+type CommandType string
+type StatusCode string
+
 type Error struct {
-	Message    string
-	StatusCode string
+	message    string
+	statusCode StatusCode
 }
 
 func (e *Error) Error() string {
-	return e.Message
+	return e.message
 }
 
-type Identifier interface {
-	~int | ~string
-}
-
-type Entity interface {
-	ID() Identifier
-	setID(id Identifier)
+func (e *Error) StatusCode() StatusCode {
+	return e.statusCode
 }
 
 type Command interface {
-	Type() string
+	Type() CommandType
+	IsValid() (error, bool)
 }
 
 type Event interface {
-	Type() string
+	Type() EventType
 }
+
+type Entity interface {
+	ID() string
+	SetID(id string)
+	Events() []Event
+}
+
+type BaseEntity struct {
+	id     string
+	events []Event
+}
+
+func (e *BaseEntity) ID() string {
+	return e.id
+}
+
+func (e *BaseEntity) SetID(id string) {
+	e.id = id
+}
+
+func (e *BaseEntity) Events() []Event {
+	return e.events
+}
+
+var _ Entity = (*BaseEntity)(nil)

709004ab142d9331774b913aec7297082da01122
victor <victor.klapholz@gmail.com>
2022-12-26 22:54:19 +0200

    almost full framework without bootstrapper.go

---
+++ pkg/service_layer/factories.go
@@ -0,0 +1,59 @@
+package service_layer
+
+import (
+	"fmt"
+	"github.com/vklap.go-ddd/pkg/domain"
+	"sync"
+)
+
+type CreateCommandHandler func() (CommandHandler, error)
+type CreateEventHandler func() (EventHandler, error)
+
+type CommandHandlerFactory struct {
+	mu               sync.Mutex
+	handlerFactories map[string]CreateCommandHandler
+}
+
+func (f *CommandHandlerFactory) Register(command domain.Command, factory CreateCommandHandler) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	f.handlerFactories[command.CommandName()] = factory
+}
+
+func (f *CommandHandlerFactory) CreateHandler(command domain.Command) (CommandHandler, error) {
+	factory, ok := f.handlerFactories[command.CommandName()]
+	if ok == false {
+		panic(fmt.Sprintf("command is not registered in executor: %q", command.CommandName()))
+	}
+	handler, err := factory()
+	if err != nil {
+		return nil, err
+	}
+	return handler, nil
+}
+
+type EventHandlersFactory struct {
+	mu               sync.Mutex
+	handlerFactories map[string][]CreateEventHandler
+}
+
+func (f *EventHandlersFactory) Register(event domain.Event, factory CreateEventHandler) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	f.handlerFactories[event.EventName()] = append(f.handlerFactories[event.EventName()], factory)
+}
+
+func (f *EventHandlersFactory) CreateHandlers(event domain.Event) ([]EventHandler, error) {
+	factories := f.handlerFactories[event.EventName()]
+	handlers := make([]EventHandler, len(factories))
+	for _, factory := range factories {
+		handler, err := factory()
+		if err != nil {
+			return nil, err
+		}
+		handlers = append(handlers, handler)
+	}
+	return handlers, nil
+}
--- pkg/service_layer/message_bus.go
+++ pkg/service_layer/message_bus.go
@@ -1,1 +1,55 @@
 package service_layer
+
+import (
+	"context"
+	"github.com/vklap.go-ddd/pkg/domain"
+	"log"
+)
+
+type MessageBus struct {
+	commandHandlerFactory CommandHandlerFactory
+	eventHandlersFactory  EventHandlersFactory
+	events                []domain.Event
+}
+
+func (m *MessageBus) Handle(ctx context.Context, command domain.Command) (any, error) {
+	handler, err := m.commandHandlerFactory.CreateHandler(command)
+	if err != nil {
+		return nil, err
+	}
+
+	uow := CommandUnitOfWork{handler}
+	result, err := uow.HandleCommand(ctx, command)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entity := range handler.SavedEntities() {
+		m.events = append(m.events, entity.Events()...)
+	}
+
+	m.handleEvents(ctx)
+
+	return result, nil
+}
+
+func (m *MessageBus) handleEvents(ctx context.Context) {
+	for len(m.events) > 0 {
+		var event domain.Event
+		event, m.events = m.events[0], m.events[1:]
+		handlers, err := m.eventHandlersFactory.CreateHandlers(event)
+		if err != nil {
+			log.Printf("failed to create event handler for %q: %v", event.EventName(), err)
+		}
+		for _, handler := range handlers {
+			uow := EventUnitOfWork{handler}
+			err = uow.HandleEvent(ctx, event)
+			if err != nil {
+				log.Printf("failed to handle event %q: %v", event.EventName(), err)
+			}
+			for _, entity := range handler.SavedEntities() {
+				m.events = append(m.events, entity.Events()...)
+			}
+		}
+	}
+}

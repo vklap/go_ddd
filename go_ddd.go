250f9453d46e8a4528639effec005232f6bd46b6
Victor Klapholz <victor@adaptive-shield.com>
2022-12-30 20:24:02 +0200

    add public func to go_ddd.go

--- go_ddd.go
+++ go_ddd.go
@@ -3,8 +3,13 @@ package go_ddd
 import (
 	"fmt"
 	_ "github.com/vklap/go_ddd/pkg/ddd"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello GO")
+}
+
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
 }

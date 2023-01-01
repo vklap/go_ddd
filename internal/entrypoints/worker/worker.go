5dc6253c0c9446ff5b76d8931db5aff36ec84a1b
Victor Klapholz <victor@adaptive-shield.com>
2023-01-01 19:03:02 +0200

    log in lowercase

--- internal/entrypoints/worker/worker.go
+++ internal/entrypoints/worker/worker.go
@@ -19,7 +19,7 @@ func Start() {
 		var command command_model.ChangeEmailCommand
 		err = json.Unmarshal(message, &command)
 		if err != nil {
-			log.Printf("Failed to unmarshal ChangeEmailCommand: %v (message: %v)", err, message)
+			log.Printf("failed to unmarshal ChangeEmailCommand: %v (message: %v)", err, message)
 			continue
 		}
 		_, err = boostrapper.HandleCommand[*command_model.ChangeEmailCommand](context.Background(), &command)
